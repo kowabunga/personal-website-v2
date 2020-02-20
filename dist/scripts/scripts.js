273591aea9766c3bbeab0e7c40c0143c503346ac
Anthony <anthony.siletti@gmail.com>
2020-02-20 14:09:45 -0500

    added css transitions instead of animations

---
+++ dist/scripts/scripts.js
@@ -0,0 +1,20 @@
+// get constant update for year
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+//dropdown toggle
+const menubtn = document.getElementById('menubtn'),
+  navbar = document.getElementById('nav');
+
+// nav dropdown click
+menubtn.addEventListener('click', () => {
+  if (!navbar.classList.contains('responsiveIn')) {
+    navbar.classList.add('responsiveIn');
+  } else {
+    navbar.classList.remove('responsiveIn');
+    navbar.classList.add('responsiveOut');
+
+    setTimeout(() => {
+      navbar.classList.remove('responsiveOut');
+    }, 401);
+  }
+});
