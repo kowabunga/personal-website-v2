d0e48967323a4ae95a37e5688a3761688a47000e
Anthony <anthony.siletti@gmail.com>
2020-02-22 23:36:29 -0500

    mobile nav animations

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -11,12 +11,22 @@ const menubtn = document.getElementById('menubtn'),
 menubtn.addEventListener('click', () => {
   if (!navbar.classList.contains('responsiveIn')) {
     navbar.classList.add('responsiveIn');
+    // add li in class 300 ms into nav bar scroll down
+    setTimeout(() => {
+      navbar.classList.add('liIn');
+    }, 300);
   } else {
+    // remove responsive "in" classes
     navbar.classList.remove('responsiveIn');
+    navbar.classList.remove('liIn');
+
+    // add responsive "out" classes, with li 300ms afterwards
     navbar.classList.add('responsiveOut');
+    navbar.classList.add('liOut');
 
     setTimeout(() => {
       navbar.classList.remove('responsiveOut');
+      navbar.classList.remove('liOut');
     }, 401);
   }
 });
